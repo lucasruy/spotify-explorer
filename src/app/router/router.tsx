b51c703a762c42468379db71d242325db6302cff
lucasruy <lucasplet@gmail.com>
2025-10-31 00:10:03 -0300

    feat: cria configuração básica do react-router

---
+++ src/app/router/router.tsx
@@ -0,0 +1,15 @@
+import { createBrowserRouter } from 'react-router-dom';
+
+import { HomePage, NotFoundPage } from '@/pages';
+
+export const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <HomePage />,
+    errorElement: <NotFoundPage />,
+  },
+  {
+    path: '*',
+    element: <NotFoundPage />,
+  },
+]);
