c45b503eb5c9916ad2869fd097f6ce4c3f037d81
lucasruy <lucasplet@gmail.com>
2025-10-31 00:09:15 -0300

    feat: cria páginas home e 404 base

---
+++ src/pages/home-page.tsx
@@ -0,0 +1,10 @@
+export const HomePage = () => {
+  return (
+    <div>
+      <div>
+        <h1>Spotify Explorer</h1>
+        <p>Explore artistas, faixas e descubra seus favoritos.</p>
+      </div>
+    </div>
+  );
+};
---
+++ src/pages/index.ts
@@ -0,0 +1,2 @@
+export { HomePage } from './home-page';
+export { NotFoundPage } from './not-found-page';
