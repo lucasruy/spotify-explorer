d7d0862f910d12e2c5a4b1d59f5d6da1398b780b
lucasruy <lucasplet@gmail.com>
2025-11-01 22:23:10 -0300

    feat: cria o componente Button

---
+++ src/shared/ui/button/button.types.ts
@@ -0,0 +1,8 @@
+import type { VariantProps } from "class-variance-authority";
+import type { buttonVariants } from "./button.variants";
+
+export interface ButtonProps
+  extends React.ButtonHTMLAttributes<HTMLButtonElement>,
+    VariantProps<typeof buttonVariants> {
+  asChild?: boolean;
+}
