6f8c00071af9845bdcbb7b28ca4860d982eaedde
lucasruy <lucasplet@gmail.com>
2025-11-03 00:20:14 -0300

    fix: adiciona tipos para ThemeOptions ao export da camada /shared/ui

--- src/shared/ui/index.ts
+++ src/shared/ui/index.ts
@@ -3,4 +3,4 @@ export { Card } from './card';
 export { Pagination } from './pagination';
 export { LanguageSelector } from './language-selector';
 export { Input } from './input';
-export { ThemeToggle } from './theme-toggle';
+export { ThemeToggle, type ThemeOptions } from './theme-toggle';
